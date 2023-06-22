049ef518d2636dcc1e58015b32a57fd633908a1a
hgy <1500586026@qq.com>
2023-06-22 19:23:31 +0800

    配置prettier eslint

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  useTabs:false,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  arrowParens: "avoid",
+  // 对象、数组括号与文字间添加空格
+  bracketSpacing: true,
+  // 尾随逗号
+  trailingComma:'none'
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+  // root: './',
+  // base: '/',
+  // publicDir: 'src',
+  resolve: {
+    alias: {
+      '@':path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    host: 'localhost',
+    port: 9000,
+    proxy: {
+      '/api':'http://api-driver.marsview.cc'
+    }
+  },
   plugins: [react()],
 })
